0df65c9c19820e62f7625f6c895e81476da050cc
Vladislav <vvkovyazin@miem.hse.ru>
2021-05-16 20:54:19 +0300

    connect to taiga project tracking and users api

--- taiga_miem_middleware/apps.py
+++ taiga_miem_middleware/apps.py
@@ -1,8 +1,19 @@
 from django.apps import AppConfig
-
+from django.conf.urls import include, url
 
 class TaigaMiemMiddlewareConfig(AppConfig):
     name = 'taiga_miem_middleware'
 
     def ready(self):
+        from taiga.base import routers
+        from taiga.urls import urlpatterns
+
+        from .projects.api import TrackingProjectsViewSet
+        from .users.api import UsersViewSet
+
+        router = routers.DefaultRouter(trailing_slash=False)
+        router.register('projects', TrackingProjectsViewSet, base_name="tracking_projects")
+        router.register(r'projects/(?P<resource_id>\d+)/users', UsersViewSet, base_name="project_users")
+        
+        urlpatterns.append(url(r'^miem/api/', include(router.urls)))
         print("MIEM Middleware Ready!")
