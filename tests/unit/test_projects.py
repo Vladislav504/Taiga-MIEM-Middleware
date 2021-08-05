46a11cbb343dc1320f851bece304b9399a47c4a3
Vladislav Kovyazin <vvkovyazin@miem.hse.ru>
2021-08-05 15:31:35 +0300

    add test for exporting multiple projects

--- tests/unit/test_projects.py
+++ tests/unit/test_projects.py
@@ -9,7 +9,7 @@ pytestmark = pytest.mark.django_db(transaction=True)
 
 def test_import_taiga_project():
     taiga_project = f.create_project(slug='111', name="111 Full name")
-    call_command('export_project_from_taiga')
+    call_command('export_project_from_taiga', '-project_numbers', 111)
     track_project = TrackingProjects.objects.filter(number=111)
     assert track_project.exists()
     assert track_project.first().project == taiga_project
@@ -17,6 +17,14 @@ def test_import_taiga_project():
 
 def test_import_taiga_project_with_invalid_name():
     f.create_project(slug='111-1', name="111-1 Incorrect name")
-    call_command('export_project_from_taiga')
+    call_command('export_project_from_taiga', all=True)
     query = TrackingProjects.objects.all()
     assert len(query) == 0
+
+
+def test_import_multiple_taiga_projects():
+    f.create_project(slug='111', name="111 Full name")
+    f.create_project(slug='112', name="112 Full name")
+    call_command('export_project_from_taiga', '-project_numbers', 111, 112)
+    track_project = TrackingProjects.objects.all()
+    assert track_project.count() == 2
